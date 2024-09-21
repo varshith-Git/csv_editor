d3da14a76b20fc9ba5ca5280d110d52dd5bb5af9
varshith G <varshith.gudur17@gmail.com>
2024-09-21 12:32:46 +0530

    final version released"

--- csv_editor.py
+++ csv_editor.py
@@ -10,6 +10,7 @@ hide_streamlit_style = """
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     header {visibility: hidden;}
+    .viewerBadge_container__1QSob {visibility: hidden;}
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
